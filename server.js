82d3a9c914d9bcadce1d448a04a7cff979e3ac0c
Dylan Caldwell <dylan.caldwell35@gmail.com>
2023-05-05 21:08:37 -0400

    server init

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const app = express();
+
+// Imports
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/client/build'));
+app.use(cookieParser());
+
+// Main route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started on port 3000');
+});
