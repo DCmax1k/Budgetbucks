bdeac32f7b96fbb680e50dd0f817363e001f61f8
Dylan Caldwell <dylan.caldwell35@gmail.com>
2023-05-10 15:28:23 -0400

    change index link name

--- client/src/components/IndexPage.js
+++ client/src/components/IndexPage.js
@@ -10,7 +10,7 @@ class IndexPage extends Component {
                 <div className='titleLogo'>
                     <img  src='/images/titleLogo.svg' alt='Budget Bucks' />
                 </div>
-                Navigate to /dashboard to test the app, or get there by clicking this link that will magically take you there without having to relad your page: <Link style={{color: "red"}} to={"/dashboard"}>mac virus</Link>
+                Navigate to /dashboard to test the app, or get there by clicking this link that will magically take you there without having to relad your page: <Link style={{color: "pink"}} to={"/dashboard"}>Dashboard</Link>
                 
             </div>
         )
